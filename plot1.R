e1558ae34a63431706ceddd15f515e7f68d1556a
prabeshjoshi <prabesh.joshi233@gmail.com>
2015-05-09 08:23:58 +0545

    plot1

--- plot1.R
+++ plot1.R
@@ -1,17 +1,17 @@
 
 household_power_consumption <- read.csv("~/diet_data/household_power_consumption.txt", sep=";")
 
-hh<-household_power_consumption
+hh0<-household_power_consumption
 
-h1<-hh[hh$Date == c("1/2/2007"),]
- h2<-hh[hh$Date=="2/2/2007",]
- data<-rbind(h1,h2)
+h10<-hh[hh$Date == "1/2/2007",]
+ h20<-hh[hh$Date=="2/2/2007",]
+ data0<-rbind(h1,h2)
 
-ga<-data$Global_active_power
-ggg<-ga[ga!='?']
-gap<-as.numeric(ggg)/500
+ga0<-data$Global_active_power
+ggg0<-ga[ga!='?']
+gap0<-as.numeric(ggg)
 
-hist(gap,xlab='kilowatts', col=2,main = 'Global Active Power')
+hist(gap0,xlab='kilowatts', col=2,main = 'Global Active Power')
 
 dev.copy(png, "plot1.png")
 dev.off()
