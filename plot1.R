09802bf74f05e0c6d344d1cb28e0c972ff8d7375
prabeshjoshi <prabesh.joshi233@gmail.com>
2015-05-09 08:07:54 +0545

    # code for plot1.png

--- plot1.R
+++ plot1.R
@@ -1,8 +1,9 @@
+
 household_power_consumption <- read.csv("~/diet_data/household_power_consumption.txt", sep=";")
 
 hh<-household_power_consumption
 
-h1<-hh[hh$Date=="1/2/2007",]
+h1<-hh[hh$Date == c("1/2/2007"),]
  h2<-hh[hh$Date=="2/2/2007",]
  data<-rbind(h1,h2)
 
